ff6d4f823675de4610beee3c30acb88c3c6cd026
willwagner602 <willwagner602@gmail.com>
2015-07-10 11:41:24 -0700

    - Added basic logic to create and populate board with pieces and impassable spaces

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
 __author__ = 'wwagner'
 
 import unittest
+import Thud
+
 
 class BoardTest(unittest.TestCase):
 
-    def test_move_piece_success(self):
-        self.assertTrue(piece.move('valid space'))
+    def test_populated_board(self):
+        test = Thud.Board()
+        test.print_board()
 
-    def test_move_piece_failure(self):
-        self.assertFalse(piece.move('invalid space'))
+
+
+# class PieceTest(unittest.TestCase)
+#     def test_move_piece_success(self):
+#         self.assertTrue(piece.move('valid space'))
+#
+#     def test_move_piece_failure(self):
+#         self.assertFalse(piece.move('invalid space'))
