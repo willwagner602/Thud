9dbf5ccdf76f3935e5eb3949f52a271ddfa9e207
willwagner602 <willwagner602@gmail.com>
2016-01-11 10:16:41 -0500

    - Updated for compatibility with Django

--- urls.py
+++ urls.py
@@ -1,16 +1,16 @@
 __author__ = 'wwagner'
 
-from Thud import DjangoThudServer
+from Thud import ThudServerDjango
 from django.conf.urls import url
 
 urlpatterns = [
-        url(r"^getgamebyid/([a-zA-Z0-9]+)", DjangoThudServer.get_game_by_id_handler),
-        url(r"^version", DjangoThudServer.get_version),
-        url(r"^start", DjangoThudServer.StartGameWithPlayers),
-        url(r"^move", DjangoThudServer.ExecuteMove),
-        url(r"^move/validate", DjangoThudServer.ValidateMove),
-        url(r"^game", DjangoThudServer.GetBoardState),
-        url(r"^save", DjangoThudServer.SaveGame),
-        url(r"^load", DjangoThudServer.LoadGame),
+        url(r"^getgamebyid/([a-zA-Z0-9]+)", ThudServerDjango.get_game_by_id_handler),
+        url(r"^version", ThudServerDjango.get_version),
+        url(r"^start", ThudServerDjango.StartGameWithPlayers),
+        url(r"^move", ThudServerDjango.ExecuteMove),
+        url(r"^move/validate", ThudServerDjango.ValidateMove),
+        url(r"^game", ThudServerDjango.GetBoardState),
+        url(r"^save", ThudServerDjango.SaveGame),
+        url(r"^load", ThudServerDjango.LoadGame),
         # url(r"^match/([A-Za-z0-9]+)", DjangoThudServer.PlayerConnection),
     ]
